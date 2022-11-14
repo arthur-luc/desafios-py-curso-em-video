741cdc698a64a8c8b37020551a3dae426618d147
Arthur Lucas Rodrigues <93298997+tutists@users.noreply.github.com>
2022-11-14 22:42:40 +0000

    Initial commit

---
+++ desafio05.py
@@ -0,0 +1,4 @@
+#antecessor e sucessor
+
+num = int(input('Escreva um número inteiro:\n'))
+print('Esse é o sucessor do seu número:\n{}\nE esse é o antecessor do seu número:\n{}'.format(num +1, num -1))
